41404d0c46257ee1f978e1edc558e597d1d7d5e9
je-an <mail@jeremiasandris.de>
2019-11-01 14:40:50 +0100

    Changed VehicleScenarioAddRequest to send id not vehicle

--- src/VehicleScenarioAddRequest.js
+++ src/VehicleScenarioAddRequest.js
@@ -2,11 +2,11 @@ define(["MessageType", "Failure", "TypeCheck"], function (MessageType, Failure, 
     "use strict";
     /**
      * Message for add a new vehicle to the scenario
-     * @param {Object} vehicle - vehicle, which shall be added to the scenario
+     * @param {String} id - vehicle id, which shall be added to the scenario
      */
-    var VehicleScenarioAddRequest = function (vehicle) {
+    var VehicleScenarioAddRequest = function (id) {
         this.type = MessageType.VEHICLE_SCENARIO_ADD_REQUEST;
-        this.vehicle = TypeCheck.isObject(vehicle) ? vehicle : Failure.throwTypeError("vehicle is not a object");
+        this.id = TypeCheck.isString(id) ? id : Failure.throwTypeError("id is not a string");
     };
     return VehicleScenarioAddRequest;
 });
